7ee94edc47f9e15670ed2ddc6f32d1fcc6aa46bc
chararchter <lentenis@inbox.lv>
2018-09-29 18:24:59 +0300

    Rewrote convertion as a function

--- main.R
+++ main.R
@@ -13,9 +13,12 @@ au = read.csv(file="2au.csv", header=FALSE, sep=",", col.names=column)
 al = read.csv(file="2al.csv", header=FALSE, sep=",", col.names=column)
 
 print(au)
-# print(al)
 
-au[,1] = au[,1] * 0.01 # convert l/2 to m
+convertToM = function(au){
+	au[,1] = au[,1] * 0.01 # convert l/2 to m
+}
+
+au[,1] = convertToM(au)
 
 Nmin = au[,3] / au[,2]
 dlt_Nmin = au[,3] * dlt_t / au[,2]^2
@@ -25,7 +28,6 @@ theta = 2 * asin(sin05theta) * 180 / pi
 dlt_theta = 2 * dlt_sin05theta / sqrt(1 - sin05theta^2)
 r2 = sqrt(a^2 + au[,1]^2)
 dlt_r2 = au[,1] * dlt_L * (a^2 + au[,1]^2)^(-0.5)
-
 
 au_means = data.frame(Nmin, dlt_Nmin, sin05theta, dlt_sin05theta, theta,
 	dlt_theta, r2, dlt_r2)
